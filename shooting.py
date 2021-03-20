71ec0489ce93f404fde5fdd515846e011802595f
mizuki-n-2 <mizukizhangze@gmail.com>
2021-03-20 11:55:38 +0900

    add player

--- shooting.py
+++ shooting.py
@@ -2,7 +2,23 @@ from game import *
 
 img_player = image('public/assets/img/airplane.png')
 
+SW, SH = 1, 9/16
+
+def player(p):
+  v = 0.025
+  if key(LEFT):
+    p.x -= v
+  if key(RIGHT):
+    p.x += v
+  if key(UP):
+    p.y += v
+  if key(DOWN):
+    p.y -= v
+  p.x = max(-SW+p.sx, min(SW-p.sx, p.x))
+  p.y = max(-SH+p.sy, min(SH-p.sy, p.y))
+
 def start():
   print("Hello World")
+  add(player, img_player)
 
-run(start, (0, 0.5, 0.1))
+run(start)
