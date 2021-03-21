73b145a8066f1bbf0581aa0777162b029d9dc85b
mizuki-n-2 <mizukizhangze@gmail.com>
2021-03-21 10:50:52 +0900

    add shot

--- shooting.py
+++ shooting.py
@@ -1,6 +1,7 @@
 from game import *
 
 img_player = image('public/assets/img/airplane.png')
+img_shot = image('public/assets/img/shot.png')
 
 SW, SH = 1, 9/16
 
@@ -15,10 +16,23 @@ def player(p):
   if key(DOWN):
     p.y -= v
   p.x = max(-SW+p.sx, min(SW-p.sx, p.x))
-  p.y = max(-SH+p.sy, min(SH-p.sy, p.y))
+  p.y = max(-SH + p.sy, min(SH - p.sy, p.y))
+
+  if key(X):
+    new_shot(p.x, p.y, 0.05)
+
+def new_shot(x, y, v):  
+  add(shot, img_shot, 0.05, x, y, 0, v)
+
+def shot(s):
+  s.x += s.vx
+  s.y += s.vy
 
 def start():
   print("Hello World")
-  add(player, img_player)
+
+  score()
+  size = 0.1
+  add(player, img_player, size, 0, -9 / 16)
 
 run(start)
